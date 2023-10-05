37d77473e3445018ebdf63d1c4f0b08635fec87b
huahua <194542159@qq.com>
2023-10-05 09:46:38 +0800

    feat: add element plus

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </a>
   </div>
   <HelloWorld msg="Vite + Vue" />
+  <el-button>button</el-button>
 </template>
 
 <style scoped>
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,12 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import {
+  ElButton,
+} from 'element-plus'
+
+const app = createApp(App);
+
+app.use(ElButton)
+app.mount('#app')
+
