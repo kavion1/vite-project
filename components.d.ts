ec9d8ba3a925e4a1c4039e88f06e1fb0c6d87618
huahua <194542159@qq.com>
2023-10-06 00:10:49 +0800

    feat: add element icon

--- components.d.ts
+++ components.d.ts
@@ -8,6 +8,16 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElCard: typeof import('element-plus/es')['ElCard']
+    ElContainer: typeof import('element-plus/es')['ElContainer']
+    ElFooter: typeof import('element-plus/es')['ElFooter']
+    ElForm: typeof import('element-plus/es')['ElForm']
+    ElFormItem: typeof import('element-plus/es')['ElFormItem']
+    ElHeader: typeof import('element-plus/es')['ElHeader']
+    ElInput: typeof import('element-plus/es')['ElInput']
+    ElMain: typeof import('element-plus/es')['ElMain']
+    Framwork: typeof import('./src/components/Framwork.vue')['default']
+    Fremwork: typeof import('./src/components/Fremwork.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,36 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router/index.js'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 import {
   ElButton,
+  ElHeader,
+  ElMain,
+  ElFooter,
+  ElForm,
+  ElFormItem,
+  ElInput,
+  ElCard,
 } from 'element-plus'
+
 
 const app = createApp(App);
 
-app.use(ElButton)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
-app.use(router) 
+app.use(ElButton)
+  .use(ElHeader)
+  .use(ElMain)
+  .use(ElFooter)
+  .use(ElForm)
+  .use(ElFormItem)
+  .use(ElInput)
+  .use(ElCard)
+
+
+app.use(router)
 app.mount('#app')
 
