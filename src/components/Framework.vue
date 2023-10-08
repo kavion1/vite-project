4f53918248c76a06a925c64d65f5333387cfa7c1
huahua0x <194542159@qq.com>
2023-10-09 00:45:03 +0800

    feat: framework 30%

--- src/components/Framework.vue
+++ src/components/Framework.vue
@@ -1,59 +1,18 @@
 <template>
   <div class="layout">
-    <el-container>
+    <el-container v-if="authenticated.includes($route.name)">
       <el-aside width="200px">
         <el-scrollbar>
-          <el-menu :default-openeds="['1', '3']">
-            <el-sub-menu index="1">
-              <template #title>
-                <el-icon><message /></el-icon>Navigator One
-              </template>
-              <el-menu-item-group>
-                <template #title>Group 1</template>
-                <el-menu-item index="1-1">Option 1</el-menu-item>
-                <el-menu-item index="1-2">Option 2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="Group 2">
-                <el-menu-item index="1-3">Option 3</el-menu-item>
-              </el-menu-item-group>
-              <el-sub-menu index="1-4">
-                <template #title>Option4</template>
-                <el-menu-item index="1-4-1">Option 4-1</el-menu-item>
-              </el-sub-menu>
-            </el-sub-menu>
+          <el-menu
+            :router="true"
+          >
             <el-sub-menu index="2">
               <template #title>
-                <el-icon><icon-menu /></el-icon>Navigator Two
+                <el-icon><message /></el-icon>交易管理
               </template>
               <el-menu-item-group>
-                <template #title>Group 1</template>
-                <el-menu-item index="2-1">Option 1</el-menu-item>
-                <el-menu-item index="2-2">Option 2</el-menu-item>
+                <el-menu-item index="/transaction_record">交易列表</el-menu-item>
               </el-menu-item-group>
-              <el-menu-item-group title="Group 2">
-                <el-menu-item index="2-3">Option 3</el-menu-item>
-              </el-menu-item-group>
-              <el-sub-menu index="2-4">
-                <template #title>Option 4</template>
-                <el-menu-item index="2-4-1">Option 4-1</el-menu-item>
-              </el-sub-menu>
-            </el-sub-menu>
-            <el-sub-menu index="3">
-              <template #title>
-                <el-icon><setting /></el-icon>Navigator Three
-              </template>
-              <el-menu-item-group>
-                <template #title>Group 1</template>
-                <el-menu-item index="3-1">Option 1</el-menu-item>
-                <el-menu-item index="3-2">Option 2</el-menu-item>
-              </el-menu-item-group>
-              <el-menu-item-group title="Group 2">
-                <el-menu-item index="3-3">Option 3</el-menu-item>
-              </el-menu-item-group>
-              <el-sub-menu index="3-4">
-                <template #title>Option 4</template>
-                <el-menu-item index="3-4-1">Option 4-1</el-menu-item>
-              </el-sub-menu>
             </el-sub-menu>
           </el-menu>
         </el-scrollbar>
@@ -61,15 +20,28 @@
       <el-container>
         <el-header>Header11</el-header>
         <el-main>
-          <route-view />
+          <router-view />
         </el-main>
       </el-container>
     </el-container>
+    <div v-else>
+      <router-view />
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { reactive, ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const authenticated = ref<string[]>([
+  'transactionRecord',
+])
+
 </script>
+
 
 <style scoped>
 .layout {
