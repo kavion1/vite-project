b48ea00c0e309eb4ec5dc7e841ba5d8f8cd7abbd
luyingjie <lu.yj@lesoon.com>
2023-10-13 16:57:07 +0800

    重置

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     proxy: {
       // 接口地址代理
       '/apis': {
-        target: '', // 接口的域名
+        target: 'https://www.fastmock.site/mock/97a65e642d057a4f41d4eda6b3b6d757/login', // 接口的域名
         changeOrigin: true, // 如果接口跨域，需要进行这个参数配置
         rewrite: (path) => path.replace(/^\/apis/, "")
       },
