5758ff55669f26d8abc913fe689ea670b32d22ea
huahua0x <194542159@qq.com>
2023-10-15 10:49:15 +0800

    feat: sigin rules

--- src/views/Login/signIn.vue
+++ src/views/Login/signIn.vue
@@ -1,34 +1,35 @@
 <template>
   <div>
-  <el-form ref="signInForm" :model="signInData">
-    <el-form-item prop="phone_num">
-      <el-input v-model="signInData.phone_num" placeholder="请输入手机号"></el-input>
-    </el-form-item>
-    <el-form-item prop="phone_code">
-      <div class="CodePass">
-        <el-input v-model="signInData.phone_code" clearable placeholder="请输入验证码"></el-input>
-        <el-button @click="getCode(ruleFormRef)" :disabled="countDown != 0">
-          {{ countDown == 0 ? "获取验证码" : countDown + "后重新获取" }}
-        </el-button>
-      </div>
-    </el-form-item>
-    <el-form-item prop="password">
-      <el-input v-model="signInData.password" placeholder="请输入密码"></el-input>
-    </el-form-item>
-    <el-form-item prop="password">
-      <el-input v-model="signInData.confirm" placeholder="请确认密码"></el-input>
-    </el-form-item>
-    <el-form-item class="login-form-item">
-        <el-button class="login-button" type="primary" @click="handleRegister">注册</el-button>
+    <el-form ref="ruleFormRef" :model="signInData" :rules="signInRules" status-icon>
+      <el-form-item prop="phone_num">
+        <el-input v-model="signInData.phone_num" placeholder="请输入手机号"></el-input>
       </el-form-item>
-  </el-form>
-</div>
+      <el-form-item prop="phone_code">
+        <div class="CodePass">
+          <el-input v-model="signInData.phone_code" clearable placeholder="请输入验证码"></el-input>
+          <el-button @click="getCode(ruleFormRef)" :disabled="countDown != 0">
+            {{ countDown == 0 ? "获取验证码" : countDown + "后重新获取" }}
+          </el-button>
+        </div>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input v-model="signInData.password" placeholder="请输入密码"></el-input>
+      </el-form-item>
+      <el-form-item prop="confirm">
+        <el-input v-model="signInData.confirm" placeholder="请确认密码"></el-input>
+      </el-form-item>
+      <el-form-item class="login-form-item">
+          <el-button class="login-button" type="primary" @click="handleRegister(ruleFormRef)">注册</el-button>
+        </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, reactive, getCurrentInstance, defineProps } from "vue";
+import { ref, reactive, getCurrentInstance, defineProps, inject, watch } from "vue";
 const { proxy } = getCurrentInstance() as any;
 import { Md5 } from "ts-md5";
+import type { FormInstance, FormRules } from 'element-plus';
 
 interface SignInData {
 	phone_num: string;
@@ -45,6 +46,40 @@ let signInData = ref<SignInData>({
   confirm: ''
 });
 
+const ruleFormRef = ref<FormInstance>();
+
+const signInRules = reactive<FormRules<LoginData>>({
+  phone_num: [
+    {
+      required: true,
+      message: '请输入手机号',
+      trigger: 'blur',
+    },
+  ],
+  phone_code: [
+    {
+      required: true,
+      message: '请输入验证码',
+      trigger: 'blur',
+    },
+  ],
+  password: [
+    {
+      required: true,
+      message: '请输入密码',
+      trigger: 'blur',
+    },
+  ],
+  confirm: [
+    {
+      required: true,
+      message: '请确认密码',
+      trigger: 'blur',
+    },
+  ],
+
+});
+
 
 const countDown = ref<number>(0);
 
@@ -53,50 +88,66 @@ const emits = defineEmits(['getActiveName']);
 const getCode = (FormRules: any) => {
 	FormRules.validateField("phone_num", (bool: boolean, b) => {
 		if (bool) {
-			countDown.value = 60;
-			const Time = setInterval(() => {
-				countDown.value = countDown.value - 1;
-				if (countDown.value == 0) {
-					clearTimeout(Time);
-				}
-			}, 1000);
+			proxy.$axios
+				?.get("/apis/api/1.0/user/smsCode", { phone_num: signInData.value.phone_num })
+				.then((result: any) => {
+					if (result.re_code == 0) {
+						countDown.value = 60;
+						const Time = setInterval(() => {
+							countDown.value = countDown.value - 1;
+							if (countDown.value == 0) {
+								clearTimeout(Time);
+							}
+						}, 1000);
+					}
+				})
+				.catch((err: any) => {
+					console.log("验证码错误", err);
+				});
 		}
 	});
 }
 
-const handleRegister = () => {
-  console.log('shuang signInData', signInData.value)
-  const params = {
-    phone_num: signInData._value.phone_num,
-    phone_code: signInData._value.password,
-    password: signInData._value.password,
-  }
-  proxy?.$axios.post('/apis/api/1.0/user/register', {
-    ...params,
-    password: Md5.hashStr(signInData._value.password)
-  }).then( res => {
+const handleRegister = async (formEl: FormInstance | undefined) => {
+  if (!formEl) return
+  await formEl.validate((valid, fields) => {
+    if (valid) {
+      const params = {
+        phone_num: signInData._value.phone_num,
+        phone_code: signInData._value.password,
+        password: signInData._value.password,
+      }
+      proxy?.$axios.post('/apis/api/1.0/user/register', {
+        ...params,
+        password: Md5.hashStr(signInData._value.password)
+      }).then( res => {
 
-    if (res.re_code === '0') {
-      signInData = {
-        phone_num: "",
-        phone_code: "",
-        password: "",
-        confirm: '',
-      };
-      ElMessage({
-        message: '注册成功',
-        type: 'success',
+        if (res.re_code === '0') {
+          signInData = {
+            phone_num: "",
+            phone_code: "",
+            password: "",
+            confirm: '',
+          };
+          ElMessage({
+            message: '注册成功',
+            type: 'success',
+          });
+          setTimeout(() => {
+              emits('getActiveName', 'login');
+          }, 500);
+        } else {
+          ElMessage({
+            message: res.msg || '注册失败',
+            type: 'error',
+          });
+        }
       });
-      setTimeout(() => {
-          emits('getActiveName', 'login');
-      }, 500);
     } else {
-      ElMessage({
-        message: res.msg || '注册失败',
-        type: 'error',
-      });
+      console.log('error handleRegister!', fields)
     }
-  });
+
+  })
 };
 </script>
 
